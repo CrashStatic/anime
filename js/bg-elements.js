215980771265bd748819717b181a005b2748c222
CrashStatic <thecrashstatic@gmail.com>
2024-10-22 18:22:24 +0300

    Работает с фильтрацие данных, полученных из БД

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,11 +1,9 @@
 const bgElements = () => {
-    const elements = document.querySelectorAll('.set-bg')
+    const elements = document.querySelectorAll('.set-bg');
 
-    for (let i = 0; i < elements.length; i++) {
-        const src = elements[i].dataset.setbg;
-
-        elements[i].style.backgroundImage = `url(${src})`;
-    }
+    elements.forEach(item => {
+        item.style.backgroundImage = `url(${item.dataset.setbg})`;
+    })
 }
 
 bgElements();
--- js/main-data.js
+++ js/main-data.js
@@ -1,10 +1,43 @@
 const mainData = () => {
+
+    const renderAnimeList = (array, ganres) => {
+        console.log(array);
+        console.log(ganres);
+    }
+
+    const renderTopAnime = (array) => {
+        const wrapper = document.querySelector('.filter__gallery');
+        wrapper.innerHTML = '';
+
+        array.forEach((item) => {
+            wrapper.insertAdjacentHTML('beforeend', `
+                <div class="product__sidebar__view__item set-bg" data-setbg="${item.image}">
+                    <div class="ep">${item.rating} / 10</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                    <h5><a href="/anime-details.html">${item.title}</a></h5>
+                </div>
+            `);
+        });
+
+		wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+			elem.style.backgroundImage = `url(${elem.dataset.setbg})`
+		})
+    }
+
     fetch('https://anime-97035-default-rtdb.firebaseio.com/anime.json')
         .then((responce) => {
             return responce.json();
         })
         .then((data) => {
-            console.log(data)
+            const ganres = new Set();
+
+            renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+
+            data.forEach((item) => {
+                ganres.add(item.ganre);
+            })
+
+            renderAnimeList(data, ganres);
         })
 };
 
